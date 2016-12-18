eda1effcc5604a09369aa366d102337184b6ae01
liujun <546064298@qq.com>
2016-12-18 23:37:24 +0800

    添加注释信息

--- Mycat-Memory/src/main/java/io/mycat/memory/ByteBufferPage.java
+++ Mycat-Memory/src/main/java/io/mycat/memory/ByteBufferPage.java
@@ -4,80 +4,159 @@ import java.nio.ByteBuffer;
 import java.util.BitSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import sun.nio.ch.DirectBuffer;
-
 /*
  * 用来保存一个一个ByteBuffer为底层存储的内存页
  */
 @SuppressWarnings("restriction")
 public class ByteBufferPage {
-    // sun.nio.ch.DirectBuffer
+    /**
+     * 缓冲区信息
+    * @字段说明 buf
+    */
     private final ByteBuffer buf;
+
+    /**
+     * 每个chunk的大小
+    * @字段说明 chunkSize
+    */
     private final int chunkSize;
+
+    /**
+     * chunk的数量
+    * @字段说明 chunkCount
+    */
     private final int chunkCount;
+
+    /**
+     * 用来进行内存位标识是否使用的集合
+    * @字段说明 chunkAllocateTrack
+    */
     private final BitSet chunkAllocateTrack;
+
+    /**
+     * 当前的锁定的状态
+    * @字段说明 allocLockStatus
+    */
     private final AtomicBoolean allocLockStatus = new AtomicBoolean(false);
+
+    /**
+     * 直接内存块的首地址信息
+    * @字段说明 startAddress
+    */
     private final long startAddress;
 
     public ByteBufferPage(ByteBuffer buf, int chunkSize) {
         super();
+        // 对本页中的chunk的大小进行设置
         this.chunkSize = chunkSize;
+        // 计算得到多少个chunk，即总容量除以每个chunk的大小
         chunkCount = buf.capacity() / chunkSize;
+        // 大小可动态改变, 取值为true或false的位集合。用于表示一组布尔标志。
+        // chunkCount指定此bitSet集合中的大小
         chunkAllocateTrack = new BitSet(chunkCount);
+        // 当前的内存缓冲区
         this.buf = buf;
+        // 直接内存的开始地址信息
         startAddress = ((sun.nio.ch.DirectBuffer) buf).address();
     }
 
+    /**
+     * 分配指定数量的chunk
+    * 方法描述
+    * @param theChunkCount 需要的chunk数量
+    * @return
+    * @创建日期 2016年12月18日
+    */
     public ByteBuffer allocatChunk(int theChunkCount) {
+
+        // 将当前进lock进行锁定
         if (!allocLockStatus.compareAndSet(false, true)) {
             return null;
         }
+        // 开始的 chunk编号
         int startChunk = -1;
+        // 继续的chunk的编号
         int contiueCount = 0;
         try {
+            // 在一个内存页的chunk数量中进宪遍历
             for (int i = 0; i < chunkCount; i++) {
+
+                // 获取当前内存坏是否已经使用,如果当前未使用
                 if (chunkAllocateTrack.get(i) == false) {
+                    // 如果当前的chunk地址未使用
                     if (startChunk == -1) {
+                        // 设置开始chunk编号为0
                         startChunk = i;
+                        // 继续的编号为1
                         contiueCount = 1;
+
+                        // 如果是仅需要分配一个则结束遍历
                         if (theChunkCount == 1) {
                             break;
                         }
-                    } else {
+                    }
+                    // 如果首个地址已经使用,检查分配的chunk能否满足需要的chunk数量
+                    else {
                         if (++contiueCount == theChunkCount) {
                             break;
                         }
                     }
-                } else {
+                }
+                // 如果已经使用，则将标识记录为下一次的开始
+                else {
                     startChunk = -1;
                     contiueCount = 0;
                 }
             }
+
+            // 如果找到的chunk数量能满足分配 要求
             if (contiueCount == theChunkCount) {
+                // 设置开始的chunk编号
                 int offStart = startChunk * chunkSize;
+                // 设置结束的chunk编号
                 int offEnd = offStart + theChunkCount * chunkSize;
+                // 将buf中的标识设置为开始的索引号
                 buf.position(offStart);
+                // 设置结束的索引号
                 buf.limit(offEnd);
                 ByteBuffer newBuf = buf.slice();
-                //sun.nio.ch.DirectBuffer theBuf = (DirectBuffer) newBuf;
-                //System.out.println("offAddress " + (theBuf.address() - startAddress));
+                // sun.nio.ch.DirectBuffer theBuf = (DirectBuffer) newBuf;
+                // System.out.println("offAddress " + (theBuf.address() -
+                // startAddress));
+                // 将内存标识位设置为已经使用
                 markChunksUsed(startChunk, theChunkCount);
                 return newBuf;
             } else {
-                //System.out.println("contiueCount " + contiueCount + " theChunkCount " + theChunkCount);
+                // System.out.println("contiueCount " + contiueCount + "
+                // theChunkCount " + theChunkCount);
                 return null;
             }
         } finally {
+            // 将当前的状态解锁
             allocLockStatus.set(false);
         }
     }
 
+    /**
+     * 将bitset中一段标识为已经使用
+    * 方法描述
+    * @param startChunk
+    * @param theChunkCount
+    * @创建日期 2016年12月18日
+    */
     private void markChunksUsed(int startChunk, int theChunkCount) {
         for (int i = 0; i < theChunkCount; i++) {
             chunkAllocateTrack.set(startChunk + i);
         }
     }
 
+    /**
+     * 找到标识信息，行清理操作 
+    * 方法描述
+    * @param startChunk 开始的内存块
+    * @param theChunkCount  清理的内存块数
+    * @创建日期 2016年12月18日
+    */
     private void markChunksUnused(int startChunk, int theChunkCount) {
         for (int i = 0; i < theChunkCount; i++) {
             chunkAllocateTrack.clear(startChunk + i);
@@ -86,14 +165,18 @@ public class ByteBufferPage {
 
     public boolean recycleBuffer(ByteBuffer parent, int startChunk, int chunkCount) {
 
+        // 检查当前的buffer是否与传递过来的parent对象相同
         if (parent == this.buf) {
 
+            // 对当前的的操作进行加锁操作,如果加锁失败，则执行其他线程
             while (!this.allocLockStatus.compareAndSet(false, true)) {
                 Thread.yield();
             }
             try {
+                // 进行标识的清理
                 markChunksUnused(startChunk, chunkCount);
             } finally {
+                // 释放标识
                 allocLockStatus.set(false);
             }
             return true;
--- Mycat-Memory/src/main/java/io/mycat/memory/DirectByteBufferPool.java
+++ Mycat-Memory/src/main/java/io/mycat/memory/DirectByteBufferPool.java
@@ -12,35 +12,80 @@ import sun.nio.ch.DirectBuffer;
 @SuppressWarnings("restriction")
 public class DirectByteBufferPool {
 
+    /**
+     * 固定的内存页的数组 
+    * @字段说明 allPages
+    */
     private ByteBufferPage[] allPages;
+
+    /**
+     * 每个内存页中数据项的大小
+    * @字段说明 chunkSize
+    */
     private final int chunkSize;
+
     private int prevAllocatedPage = 0;
 
+    /**
+    * 构造方法
+    * @param pageSize 每个内存页的大小
+    * @param chunkSize 每个内存页中chunk的大小
+    * @param pageCount 总的内存页数
+    */
     public DirectByteBufferPool(int pageSize, short chunkSize, short pageCount) {
+        // 保存所有的内存页的数组
         allPages = new ByteBufferPage[pageCount];
+        // 每个内存页中chunk的大小
         this.chunkSize = chunkSize;
+        // 进行初始化每个内存页数据的分配
         for (int i = 0; i < pageCount; i++) {
             allPages[i] = new ByteBufferPage(ByteBuffer.allocateDirect(pageSize), chunkSize);
         }
     }
 
+    /**
+     * 构造一个内存的byteBuffer对象
+    * 方法描述
+    * @param size
+    * @return
+    * @创建日期 2016年12月18日
+    */
     public ByteBuffer allocate(int size) {
+
+        // 计算需要的chunk的数量
         int theChunkCount = size / chunkSize + (size % chunkSize == 0 ? 0 : 1);
+        // 得到结束的内存页索引号
         int selectedPage = (++prevAllocatedPage) % allPages.length;
+        // 进行内存分配
         ByteBuffer byteBuf = allocateBuffer(theChunkCount, 0, selectedPage);
+        // 如果内存分配失败，则再进一次内存分配
         if (byteBuf == null) {
             byteBuf = allocateBuffer(theChunkCount, selectedPage, allPages.length);
         }
         return byteBuf;
     }
 
+    /**
+     * 进行内存回收
+    * 方法描述
+    * @param theBuf
+    * @创建日期 2016年12月18日
+    */
     public void recycle(ByteBuffer theBuf) {
+        // 初始化内存回收为false
         boolean recycled = false;
+        // 获得内存buffer
         sun.nio.ch.DirectBuffer thisNavBuf = (DirectBuffer) theBuf;
+        // 计算得到总的chunk数
         int chunkCount = theBuf.capacity() / chunkSize;
+        // attachment对象在buf.slice();的时候将attachment对象设置为总的buff对象
         sun.nio.ch.DirectBuffer parentBuf = (DirectBuffer) thisNavBuf.attachment();
+        // 已经使用的地址减去父类最开始的地址，即为所有已经使用的地址，除以chunkSize得到chunk当前开始的地址,得到整块内存开始的地址
         int startChunk = (int) ((thisNavBuf.address() - parentBuf.address()) / this.chunkSize);
+
+        // 进行遍历内存页
         for (int i = 0; i < allPages.length; i++) {
+            // 找到内存页进行内存的归还
             if ((recycled = allPages[i].recycleBuffer((ByteBuffer) parentBuf, startChunk, chunkCount) == true)) {
                 break;
             }
@@ -50,10 +95,21 @@ public class DirectByteBufferPool {
         }
     }
 
+    /**
+     * 从指定的内存页中获取需要的chunk
+    * 方法描述
+    * @param theChunkCount 需要的chunk数量
+    * @param startPage 开始的内存页号
+    * @param endPage 结束的内存页号
+    * @return
+    * @创建日期 2016年12月18日
+    */
     private ByteBuffer allocateBuffer(int theChunkCount, int startPage, int endPage) {
         for (int i = startPage; i < endPage; i++) {
+            // 进行指定内存页中的内存空间分配
             ByteBuffer buffer = allPages[i].allocatChunk(theChunkCount);
             if (buffer != null) {
+                // 分配成功，将当前的分配标识记录为当前操作的开始页编号
                 prevAllocatedPage = i;
                 return buffer;
             }
